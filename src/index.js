3d237871b27b4da313d79a981a3d9ca2fd5dd873
Singgih <singgihnn@gmail.com>
2019-01-18 11:08:50 +0700

    add gtm

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import TagManager from 'react-gtm-module';
 
 import './index.css';
 
 import { store } from './store';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-57QG7WJ',
+};
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render(
   <Provider store={store}>
